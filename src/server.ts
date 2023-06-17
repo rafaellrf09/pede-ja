599b2b539e2c9817600927d31543fba658a06d2b
Rafael Lima <rafaellrf09@gmail.com>
2023-06-17 15:39:59 -0300

    feat: add rabbitmq and socketio on server starter

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 import { config } from "dotenv";
 import { app } from "./app";
 import { connectToMongoDB } from "./config/mongodb";
+import { connection } from "mongoose";
+import OrderMessageChannel from "./messages/OrderMessageChannel";
 
 const createServer = async () => {
     config();
-    const PORT = process.env.PORT || 3001;
-
     connectToMongoDB();
 
-    app.listen(PORT, () => {
+    const PORT = process.env.PORT || 3001;
+    const server = app.listen(PORT, () => {
         console.log(`App running on port ${PORT}`);
     });
+
+    const orderMsgChannel = new OrderMessageChannel(server);
+    await orderMsgChannel.consumeMessages();
+
+    process.on('SIGINT', async () => {
+        await connection.close();
+        server.close();
+        console.log("App server and MongoDb are closed")
+    })
 }
 
 createServer();
