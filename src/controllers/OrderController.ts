21dc645d9e14a2ef42e4759fef8716b7995629b6
Rafael Lima <rafaellrf09@gmail.com>
2023-06-17 15:40:28 -0300

    refactor: remove no used params on order Controller

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
-import { Order, Status } from "../models/Order";
+import { Status } from "../models/Order";
 import OrderService from "../services/OrderService";
+import RabbitMQClient from "../messages/RabbitMQClient";
+import { config } from 'dotenv';
 
+config();
 export default class OrderController {
 
     private _orderService: OrderService;
@@ -11,18 +14,14 @@ export default class OrderController {
     }
 
     create = async (req: Request, res: Response) => {
-        const { items, clientData, paymentMethod } = req.body;
-
-        if (!items || !clientData || !paymentMethod)
-            return res.status(400).json({
-                errorCode: 400,
-                error: `Cannot create order with no parameters: ${!items || !clientData || !paymentMethod}`
-            });
-
         const newOrder = await this._orderService.save({
             ...req.body,
             status: Status.recieved,
         });
+        
+        const orderMessageQueue = new RabbitMQClient(process.env.QUEUE_ORDERS_RECIEVED);
+        await orderMessageQueue.connect();
+        await orderMessageQueue.sendMessage(JSON.stringify(newOrder));
 
         return res.status(201).json(newOrder);
     }
