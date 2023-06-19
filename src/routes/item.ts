d799487629afd19c45ff45c2b421c741fae52675
Rafael Lima <rafaellrf09@gmail.com>
2023-06-19 13:08:13 -0300

    feat: create a new routes for items

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import ItemController from '../controllers/ItemController';
+import ItemService from "../services/ItemService";
+
 
 export const itemRouter = Router();
 
-const itemCtrl = new ItemController();
+const itemSrvc = new ItemService();
+const itemCtrl = new ItemController(itemSrvc);
 
 itemRouter.post('/items', itemCtrl.create);
+itemRouter.get('/items', itemCtrl.getAll);
+itemRouter.get('/items/:id', itemCtrl.getById);
+itemRouter.patch('/items/:id', itemCtrl.changeItemActive);
