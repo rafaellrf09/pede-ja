033b8c7a74ca10171a914900e197585fb49b756f
Rafael Lima <rafaellrf09@gmail.com>
2023-06-19 13:09:14 -0300

    feat: create interface for items methods

--- src/controllers/ItemController.ts
+++ src/controllers/ItemController.ts
@@ -1,16 +1,43 @@
 import { Request, Response } from "express";
-import ItemService from "../services/ItemService";
+import ItemService, { InterfaceItemService } from "../services/ItemService";
 
 export default class ItemController {
 
-    private _itemService: ItemService;
+    private _itemService: InterfaceItemService;
 
-    constructor() {
-        this._itemService = new ItemService();
+    constructor(itemService: InterfaceItemService) {
+        this._itemService = itemService;
     }
 
     create = async (req: Request, res: Response) => {
         const newItem = await this._itemService.save(req.body);
         return res.status(201).json(newItem);
+    }
+
+    getById = async (req: Request, res: Response) => {
+        try {
+            const item = await this._itemService.getById(req.params.id);
+            return res.json(item);
+        } catch (error) {
+            return res.status(400).json(error);
+        }
+    }
+
+    getAll = async (req: Request, res: Response) => {
+        try {
+            const item = await this._itemService.getAll();
+            return res.json(item);
+        } catch (error) {
+            return res.status(400).json(error);
+        }
+    }
+
+    changeItemActive = async (req: Request, res: Response) => {
+        try {
+            const item = await this._itemService.changeActive(req.params.id);
+            return res.status(200).json(item);
+        } catch (error) {
+            return res.status(400).json(error);
+        }
     }
 }
--- src/services/ItemService.ts
+++ src/services/ItemService.ts
@@ -1,7 +1,37 @@
+import mongoose from "mongoose";
 import { Item, ItemModel } from "../models/Item";
+
+export interface InterfaceItemService {
+    save(item: Item): Promise<Item>,
+    getAll(): Promise<Item[]>,
+    getById(id: string): Promise<Item>,
+    changeActive(id: string): Promise<Item>
+}
 
 export default class ItemService {
     async save(item: Item): Promise<Item> {
         return await ItemModel.create(item);
+    }
+
+    async getAll(): Promise<Item[]> {
+        const items: Item[] = await ItemModel.find();
+        return items;
+    }
+
+    async getById(id: string): Promise<Item> {
+        const objId = new mongoose.Types.ObjectId(id);
+        const item: Item = await ItemModel.findById(objId);
+        return item;
+    }
+
+    async changeActive(id: string): Promise<Item> {
+        const objId = new mongoose.Types.ObjectId(id);
+        const item: Item = await ItemModel.findById(objId);
+
+        item.isActive = !item.isActive;
+
+        item.save();
+
+        return item;
     }
 }
