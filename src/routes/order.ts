47b0ac1cfaf617412ad783e8f866f1b0c04c428f
Rafael Lima <rafaellrf09@gmail.com>
2023-06-20 13:56:26 -0300

    feat: add new route for all orders

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -8,5 +8,6 @@ const orderSrvc = new OrderService();
 const orderCtrl = new OrderController(orderSrvc);
 
 orderRouter.post('/orders', orderCtrl.create);
+orderRouter.get('/orders', orderCtrl.getAll);
 orderRouter.get('/orders/:id', orderCtrl.getById);
 orderRouter.put('/orders/:id/status/:status', orderCtrl.changeStatus);
